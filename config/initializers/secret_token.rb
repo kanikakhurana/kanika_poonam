2eb5fa9897a387dd3155d057b00e4a181dc0ac54
arnav90 <arnavk90@thoughtworks.com>
2012-12-08 13:44:21 +0530

    we have done with the initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KanikaPoonam::Application.config.secret_token = '8f0a8b017aabf271f3d745304792ac23bcf3c07e31a97f07e18861270422aed3f45494a039daf00278d045019abe16b633e48006f744a587a118f79e3f072b16'
